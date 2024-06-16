7e9904e97cd2359703eaa443ecacd3e16ae70fb6
Bernardo <bernactkj@gmail.com>
2024-06-16 02:26:28 -0400

    testing stuff

--- nvideos_web/main.py
+++ nvideos_web/main.py
@@ -7,19 +7,27 @@ from user_details.view import ud
 # CONFIG
 from config import load_dotenv
 
+from typing import Protocol
+
 app = Flask(__name__)
 app.config.from_file(".env", load_dotenv)
 
-class Abc:
-    def __init__(self) -> None:
-        self.id = 1
+class Ab(Protocol):
+    def test(self):
+        pass
+
+class Abc(Ab):
+    def ooo(self):
+        pass
+
+ac = Abc()
 
 app.register_blueprint(ud)
 
 @app.route("/abc/")
 def abc():
     ab = Abc()
-    session["user"] = ab
+    session["user"] = {"a": 22}
     return "Hello"
 
 @app.route("/")
