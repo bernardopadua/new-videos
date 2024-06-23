ebe7a19ba2b2ea3ecfe3eddd9881aaf4cc569c5c
Bernardo de Pádua Oliveira <bernactkj@gmail.com>
2024-06-23 17:24:43 -0400

    adding some web nvideos commands

--- nvideos.commands.sh
+++ nvideos.commands.sh
@@ -3,11 +3,14 @@
 absolute_path=$(pwd)
 export NVIDEOS_PATH="${absolute_path}"
 
-wnvideos_runlocal(){
+wnvideos.runlocal(){
     python3 -m nvideos_web
 }
-wnvideos_export_env_vars(){
+wnvideos.export_env_vars(){
     set -o allexport
     source "${NVIDEOS_PATH}/nvideos_web/.env"
     set +o allexport
+}
+wnvideos.start_postgres(){
+    sudo bash -c "docker compose up postgres -d"
 }
