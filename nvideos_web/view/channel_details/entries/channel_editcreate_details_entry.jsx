6eddf20f34a25fa5685c26efdcc3e3d01bc0eb68
Bernardo <bernactkj@gmail.com>
2026-08-18 20:46:18 -0400

    Adding do refactoring.

--- nvideos_web/view/channel_details/entries/channel_editcreate_details_entry.jsx
+++ nvideos_web/view/channel_details/entries/channel_editcreate_details_entry.jsx
@@ -6,18 +6,6 @@ import AvatarUpload from '../components/AvatarUpload';
 
 import ChannelImageUpload from './channel_image_upload/channel_image_upload_editcreate';
 
-const formChannel = document.getElementById("channel-details-form");
-formChannel.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const mediaUpload = new ChannelImageUpload();
-    const submitFormCallback = () => {
-        formChannel.submit();
-    }
-
-    mediaUpload.doUploadMedia(submitFormCallback);
-});
-
 const bannerCoverUrl = document.getElementById("bannerCoverImageUrl");
 const rootCoverImage = ReactDOM.createRoot(document.getElementById("root-cover-image"));
 rootCoverImage.render(
@@ -33,3 +21,15 @@ rootAvatar.render(
         <AvatarUpload avatarFileNameMediaServer={avatarFileNameMediaServer} />
     </React.StrictMode>
 );
+
+const formChannel = document.getElementById("channel-details-form");
+formChannel.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const mediaUpload = new ChannelImageUpload();
+    const submitFormCallback = () => {
+        formChannel.submit();
+    }
+
+    mediaUpload.doUploadMedia(submitFormCallback);
+});
